bc81d7fb8a5f7fb042557445f62ff2ac4a8ae99d
sascha-knowns <sascha@knowns.io>
2022-08-12 15:19:02 +0200

    changed database to mysql

---
+++ config/env/development/database.js
@@ -0,0 +1,17 @@
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "mysql",
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 3306),
+        database: env("DATABASE_NAME", "stra_db_Q62pnbEL"),
+        username: env("DATABASE_USERNAME", "strapi_user1"),
+        password: env("DATABASE_PASSWORD", "G5TifKq7huq8Va8svlQv9"),
+      },
+      options: {},
+    },
+  },
+});
